c23644db04408845bab38fb4c4202a96d029ba13
Hossam123546 <karem3335aa@gmail.com>
2026-08-22 18:22:39 +0300

    Delete me.js

--- me.js
+++
@@ -1,6 +0,0 @@
-const {currentUser}=require("./_auth");
-module.exports=async(req,res)=>{
-  res.setHeader("Content-Type","application/json");
-  try{res.statusCode=200;res.end(JSON.stringify({user:await currentUser(req)}))}
-  catch(e){console.error(e);res.statusCode=500;res.end(JSON.stringify({error:"تعذر قراءة الجلسة"}))}
-};
